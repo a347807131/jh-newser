f202948486e0c25e0624e54bb74e6663c1ae8107
Gatsby <civin@bupt.edu.cn>
2020-11-18 11:30:43 +0800

    updated footer component

--- src/main/webapp/app/core/jhi-footer/jhi-footer.vue
+++ src/main/webapp/app/core/jhi-footer/jhi-footer.vue
@@ -1,24 +1,22 @@
 <template>
-    <div id="footer" class="footer">
-        <p v-text="$t('footer')">This is your footer</p>
-        <el-container>
-            <el-header>Header</el-header>
-            <el-main>Main</el-main>
-        </el-container>
-    </div>
+    <!--            <p v-text="$t('footer')">This is your footer.</p>-->
+    <el-container id="'footer">
+        <el-main>
+            Main
+        </el-main>
+    </el-container>
 </template>
 
 <script lang="ts" src="./jhi-footer.component.ts">
 </script>
 
 <style scoped>
-    .footer {
-        text-align: left;
-    }
-    .el-main {
-        background-color: #E9EEF3;
-        color: #333;
-        text-align: center;
-        line-height: 160px;
-    }
+
+.footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+}
+
 </style>
